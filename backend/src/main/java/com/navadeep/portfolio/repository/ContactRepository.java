c2bfa5eb0c96ad57e704f9bee2a48af1093c764d
AVANIGADDA NAVADEEP <163722159+2300030055@users.noreply.github.com>
2026-09-05 10:58:28 +0530

    Delete backend directory

--- backend/src/main/java/com/navadeep/portfolio/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.navadeep.portfolio.repository;
-
-import com.navadeep.portfolio.entity.ContactMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository<ContactMessage, Long> {
-}
